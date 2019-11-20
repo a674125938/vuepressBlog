0783bd57e3d81e7eca50dabd460d7967b78b6756
fanqing <fanqing@enmonster.com>
2019-11-20 14:49:35 +0800

    测试

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,6 +1,6 @@
 module.exports = {
-    title: '裂泉首页', // 显示在左上角的网页名称以及首页在浏览器标签显示的title名称
-    description: '裂泉的前端记录', // meta 中的描述文字，用于SEO
+    title: '首页', // 显示在左上角的网页名称以及首页在浏览器标签显示的title名称
+    description: '前端记录', // meta 中的描述文字，用于SEO
     // 注入到当前页面的 HTML <head> 中的标签
     head: [
         ['link', { rel: 'icon', href: '/egg.png' }],  //浏览器的标签栏的网页图标
